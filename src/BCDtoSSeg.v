997ac0aa692dfd320f83158152f8a1332d5c2a6d
DianaNatali <dnmaldonador@unal.edu.co>
2025-04-02 20:24:00 -0500

    update

--- src/BCDtoSSeg.v
+++ src/BCDtoSSeg.v
@@ -1,28 +1,20 @@
-module BCDtoSSeg (
-  input [3:0] BCD, 
-  input reg [6:0] SSeg
+module multiplexor_7seg(
+  input  [3:0] BCD,  
+  output [6:0] SSeg 
 );
-
-always @(*) begin
-  case (BCD)
-    4'b0000: SSeg = 7'b0000001; // "0"  
-	  4'b0001: SSeg = 7'b1001111; // "1" 
-	  4'b0010: SSeg = 7'b0010010; // "2" 
-	  4'b0011: SSeg = 7'b0000110; // "3" 
-	  4'b0100: SSeg = 7'b1001100; // "4" 
-	  4'b0101: SSeg = 7'b0100100; // "5" 
-	  4'b0110: SSeg = 7'b0100000; // "6" 
-	  4'b0111: SSeg = 7'b0001111; // "7" 
-	  4'b1000: SSeg = 7'b0000000; // "8"  
-	  4'b1001: SSeg = 7'b0000100; // "9" 
-    4'ha: SSeg = 7'b0001000;  // "a"
-    4'hb: SSeg = 7'b1100000;  // "b"
-    4'hc: SSeg = 7'b0110001;  // "c"
-    4'hd: SSeg = 7'b1000010;  // "d" 
-    4'he: SSeg = 7'b0110000;  // "e"
-    4'hf: SSeg = 7'b0111000;  // "f"
-    default:SSeg = 7'b0000000;
-  endcase
-end
-
+  always_comb begin
+      case (BCD)
+          4'b0000: SSeg = ~ 7'b0111111; // 0
+          4'b0001: SSeg = ~ 7'b0000110; // 1
+          4'b0010: SSeg = ~ 7'b1011011; // 2
+          4'b0011: SSeg = ~ 7'b1001111; // 3
+          4'b0100: SSeg = ~ 7'b1100110; // 4
+          4'b0101: SSeg = ~ 7'b1101101; // 5
+          4'b0110: SSeg = ~ 7'b1111101; // 6
+          4'b0111: SSeg = ~ 7'b0000111; // 7
+          4'b1000: SSeg = ~ 7'b1111111; // 8
+          4'b1001: SSeg = ~ 7'b1101111; // 9
+          default: SSeg = ~ 7'b0000000; // Apagado
+      endcase
+  end
 endmodule
